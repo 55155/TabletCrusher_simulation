5beb0628ff324a5e18cc7db26d5090898fc21073
55155 <qkdtjdwls1024@gmail.com>
2025-12-01 05:43:20 -0500

    [PBD-Rigid] 20251201 19:43

--- src/PBD/Samplebag_test.py
+++ src/PBD/Samplebag_test.py
@@ -17,50 +17,81 @@ scene = gs.Scene(
     ),
     show_viewer = True,
 )
-Samplebag_filename = "/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/SAMPLEBAG_description/urdf/base_link.stl"
+Samplebag_filename = "/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Sample_bag.obj"
+
+########################## entities ##########################
+rigid_material = gs.materials.Rigid(needs_coup=True, coup_friction=0.0)
 
 ########################## entities ##########################
 plane = scene.add_entity(
     morph=gs.morphs.Plane(),
+    material=rigid_material,
 )
 
 Samplebag = scene.add_entity(
-    material=gs.materials.PBD.Elastic(),
-    morph=gs.morphs.Box(
-        size = (0.2, 0.1, 0.3),
-        pos=(0, 0, 1.0),
+    material=gs.materials.PBD.Cloth(),
+    morph=gs.morphs.Mesh(
+        file = Samplebag_filename,
+        scale=0.001,
     ),
     surface=gs.surfaces.Default(
-        color=(0.2, 0.4, 0.8, 1.0),
+        smooth=True,
+        color=(1.0, 1.0, 1.0, 1.0),
+        opacity = 0.3,
         vis_mode='visual',
     )
 )
+cam = scene.add_camera(
+    res=(1280, 960),
+    pos=(0.5, 0.5, 0.5),
+    lookat=(0.0500, 0.0500, 0.1000), 
+)
 
-# cloth_2 = scene.add_entity(
-#     material=gs.materials.PBD.Cloth(),
-#     morph=gs.morphs.Mesh(
-#         file='meshes/cloth.obj',
-#         scale=2.0,
-#         pos=(0, 0, 1.0), 
-#         euler=(0.0, 0, 0.0),
+tablet = scene.add_entity(
+    material=rigid_material,
+    morph=gs.morphs.MJCF(
+        file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.xml',
+        scale = 1.0, pos = (0,0,0.5), euler = (0,0,0), decimate = False, convexify = False,),
+    surface=gs.surfaces.Default(
+        smooth=False,
+    ),
+    vis_mode="collision",
+    visualize_contact=True,
+)
+
+# tablet = scene.add_entity(
+#     material=gs.materials.PBD.Elastic(),
+#     morph = gs.morphs.Mesh(
+#         file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.obj',
+#         scale = 0.005, pos = (0.0500, 0.0500, 0.1000), euler = (0,0,0),
 #     ),
-#     surface=gs.surfaces.Default(
-#         color=(0.8, 0.4, 0.2, 1.0),
-#         vis_mode='particle',
-#     )
 # )
 
-########################## build ##########################
 scene.build()
+cam.start_recording()
 
+a = Samplebag.find_closest_particle((-1, -1, 1.0))
+b = Samplebag.find_closest_particle((1, 1, 1.0)) 
 
-# cloth_1.fix_particles(cloth_1.find_closest_particle((-1, -1, 1.0)))
-# cloth_1.fix_particles(cloth_1.find_closest_particle((1, 1, 1.0)))
-# cloth_1.fix_particles(cloth_1.find_closest_particle((-1, 1, 1.0)))
-# cloth_1.fix_particles(cloth_1.find_closest_particle((1, -1, 1.0)))
+print("좌측상단 : ", a)
+print("우측상단 : ", b)
+print("중간상단 :", (a+b)/2)
 
-# cloth_2.fix_particles(cloth_2.find_closest_particle((-1, -1, 1.0)))
+particle_pos = Samplebag.get_particles_pos()
+left_top_pos = particle_pos[a]
+right_top_pos = particle_pos[b]
+center_top_pos = (left_top_pos + right_top_pos) / 2
+print("중앙상단 좌표 :", center_top_pos)
+
+# tablet.set_pos(center_top_pos)
+
+Samplebag.fix_particles(Samplebag.find_closest_particle((-1, -1, 1.0)))
+Samplebag.fix_particles(Samplebag.find_closest_particle((1, 1, 1.0)))
 
 horizon = 1000
 for i in range(horizon):
     scene.step()
+    if i == 500:
+        tablet.set_pos((0.0500, 0.0500, 0.1000))
+    cam.render()
+cam.stop_recording(save_to_filename="video/[20251201] Samplebag_test4.mp4")
