cccfaf21d19eb541de212d124a81e06b638e2133
55155 <qkdtjdwls1024@gmail.com>
2025-12-02 06:54:51 -0500

    [PBD-Rigid] 20251202 21:00

--- src/PBD/Samplebag_test.py
+++ src/PBD/Samplebag_test.py
@@ -7,7 +7,7 @@ gs.init()
 
 scene = gs.Scene(
     sim_options=gs.options.SimOptions(
-        dt       = 4e-3,
+        dt       = 0.01,
         substeps = 10,
     ),
     viewer_options=gs.options.ViewerOptions(
@@ -29,7 +29,7 @@ plane = scene.add_entity(
 )
 
 Samplebag = scene.add_entity(
-    material=gs.materials.PBD.Cloth(),
+    material=gs.materials.FEM.Cloth(),
     morph=gs.morphs.Mesh(
         file = Samplebag_filename,
         scale=0.001,
@@ -45,27 +45,29 @@ cam = scene.add_camera(
     res=(1280, 960),
     pos=(0.5, 0.5, 0.5),
     lookat=(0.0500, 0.0500, 0.1000), 
-)
+) 
+
+# tablet = scene.add_entity(
+#     material=rigid_material,
+#     morph=gs.morphs.MJCF(
+#         file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.xml',
+#         scale = 1.0, pos = (0,0,10), euler = (0,0,0), decimate = False, convexify = False,),
+#     surface=gs.surfaces.Default(
+#         smooth=False,
+#     ),
+#     vis_mode="collision",
+#     visualize_contact=True,
+# )
 
 tablet = scene.add_entity(
     material=rigid_material,
-    morph=gs.morphs.MJCF(
-        file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.xml',
-        scale = 1.0, pos = (0,0,0.5), euler = (0,0,0), decimate = False, convexify = False,),
-    surface=gs.surfaces.Default(
-        smooth=False,
+    morph = gs.morphs.Mesh(
+        file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.obj',
+        scale = 0.002, pos = (0.0500, 0.0500, 3.1000), euler = (0,0,0),
     ),
     vis_mode="collision",
     visualize_contact=True,
 )
-
-# tablet = scene.add_entity(
-#     material=gs.materials.PBD.Elastic(),
-#     morph = gs.morphs.Mesh(
-#         file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.obj',
-#         scale = 0.005, pos = (0.0500, 0.0500, 0.1000), euler = (0,0,0),
-#     ),
-# )
 
 scene.build()
 cam.start_recording()
@@ -88,10 +90,10 @@ print("중앙상단 좌표 :", center_top_pos)
 Samplebag.fix_particles(Samplebag.find_closest_particle((-1, -1, 1.0)))
 Samplebag.fix_particles(Samplebag.find_closest_particle((1, 1, 1.0)))
 
-horizon = 1000
+horizon = 400
 for i in range(horizon):
     scene.step()
-    if i == 500:
-        tablet.set_pos((0.0500, 0.0500, 0.1000))
+    # if i == 100:
+    #     tablet.set_pos((0.0500, 0.0500, 0.0500))
     cam.render()
-cam.stop_recording(save_to_filename="video/[20251201] Samplebag_test4.mp4")
+cam.stop_recording(save_to_filename="video/[20251202] Samplebag_test4.mp4")
