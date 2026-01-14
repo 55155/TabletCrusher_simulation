695f11c6bd502175566c531f6b69ccd415f56125
55155 <qkdtjdwls1024@gmail.com>
2026-01-14 04:32:47 -0500

    [IPC solver] Error  20260114 18:30

--- src/My_robot/IPC/IPC_solver2.py
+++ src/My_robot/IPC/IPC_solver2.py
@@ -47,7 +47,7 @@ scene = gs.Scene(
     show_viewer=args.vis,
 )
 args.vis = args.vis or args.vis_ipc
-########################## materials ##########################
+######################### materials #########################
 rigid_material = gs.materials.Rigid(needs_coup=True, coup_friction=0.0)
 cloth_material = gs.materials.PBD.Cloth()
 ######################### entities ##########################
@@ -56,51 +56,12 @@ plane = scene.add_entity(
     material=rigid_material,
 )
 
-# Samplebag = scene.add_entity(
-#     material=cloth_material,
-#     morph=gs.morphs.Mesh(
-#         file = Samplebag_filename,
-#         scale=0.001,
-#         # decimate=False,
-#         decimate_face_num=1000,
-#     ),
-#     surface=gs.surfaces.Default(
-#         smooth=True,
-#         color=(1.0, 1.0, 1.0, 1.0),
-#         opacity = 0.3,
-#         vis_mode='visual',
-#     )
-# )
 cam = scene.add_camera(
     res=(1280, 960),
     pos=(0.5, 0.5, 0.5),
     lookat=(0.0500, 0.0500, 0.1000), 
 ) 
 
-# tablet = scene.add_entity(
-#     material=rigid_material,
-#     morph=gs.morphs.MJCF(
-#         file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.xml',
-#         scale = 1.0, pos = (0,0,10), euler = (0,0,0), decimate = False, convexify = False,),
-#     surface=gs.surfaces.Default(
-#         smooth=False,
-#     ),
-#     vis_mode="collision",
-#     visualize_contact=True,
-# )
-
-# tablet = scene.add_entity(
-#     material=rigid_material,
-#     morph = gs.morphs.Mesh(
-#         file = '/home/seongjin/Desktop/Seongjin/genesis_simulation_on_linux/My_asset/Tablet_posmod/Tablet_posmod.obj',
-#         scale = 0.002, pos = (0.0500, 0.0500, 1.1000), euler = (0,0,0),
-#         decimate=True,          # 👈 추가
-#         decimate_face_num=500,  # 👈 추가: 17k→500 faces
-#         convexify=True,         # 👈 추가: 볼록 껍질로 collision 최적화
-#     ),
-#     vis_mode="collision",
-#     visualize_contact=True,
-# )
 ball = scene.add_entity(
     material=rigid_material,
     morph=gs.morphs.Sphere(
@@ -115,30 +76,11 @@ ball = scene.add_entity(
 scene.build()
 cam.start_recording()
 
-# a = Samplebag.find_closest_particle((-1, 1, 1.0))
-# b = Samplebag.find_closest_particle((1, 1, 1.0)) 
-
-# print("좌측상단 : ", a)
-# print("우측상단 : ", b)
-# print("중간상단 :", (a+b)/2)
-
-# particle_pos = Samplebag.get_particles_pos()
-# left_top_pos = particle_pos[a]
-# right_top_pos = particle_pos[b]
-# center_top_pos = (left_top_pos + right_top_pos) / 2
-# print("중앙상단 좌표 :", center_top_pos)
-
-# # tablet.set_pos(center_top_pos)
-
-# Samplebag.fix_particles(Samplebag.find_closest_particle((-1, -1, 1.0)))
-# Samplebag.fix_particles(Samplebag.find_closest_particle((1, 1, 1.0)))
-
 horizon = 5000
 for i in range(horizon):
     print(f"Step {i+1}/{horizon}")
     scene.step()
     print("Scene stepped.")
-    # if i == 100:
-    #     tablet.set_pos((0.0500, 0.0500, 0.0500))
     cam.render()
+
 cam.stop_recording(save_to_filename="./video/ipc_solver_samplebag/[20260114] Samplebag(PBD)-tablet(Rigid) 3.mp4")
